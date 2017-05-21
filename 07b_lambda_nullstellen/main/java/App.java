ea7e97db3447ae699c900a2caa35a1395e23c1ca
Tom <duke64@gmx.de>
2017-05-21 13:23:12 +0200

    Add: Excericse 7b (tbc)

---
+++ 07b_lambda_nullstellen/main/java/App.java
@@ -0,0 +1,19 @@
+/**
+ * App zur Nullstelle
+ *
+ * @author Tom Dittrich s0555944@htw-berlin.de
+ * @version 0.5
+ * @date 21.05.17
+ */
+public class App {
+    public static void main(String[] args) {
+
+        try {
+            double ergebnis = Nullstelle.suche(-30, 30);
+            System.out.println("ENDE: " + ergebnis);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
---
+++ 07b_lambda_nullstellen/main/java/Nullstelle.java
@@ -0,0 +1,56 @@
+/**
+ * Berechnet die Nullstelle nach dem
+ * Bisektions Verfahren
+ *
+ * @author Tom Dittrich s0555944@htw-berlin.de
+ * @version 0.7
+ * @date 21.05.17
+ */
+public class Nullstelle {
+
+    // Toleranz, ab wann die Nullstelle gefunden ist
+    private static final double toleranz = 0.01;
+    private static int maxIt = 20; // max. Iterationen
+    private static int zaehler = 0; // Zaehler für Iterationen
+
+    public static double f(double x) {
+        return (x * x) - 5;
+    }
+
+    /**
+     * sucht eine Nullstelle nach BiSektion
+     *
+     * @param links linke Intervallgrenze (sollte Gegenteil von rechts sein)
+     * @param rechts rechte Intervallgrenze (Gegenteil von links)
+     * @return die Nullstelle
+     * @throws Exception falls zu viele Iterationen
+     */
+    // links und rechts sollten beim Start das Gegenteil sein!
+    // z.B. "-10 und 10" oder "-5 und 5"
+    // andernfalls kann es zu Problemen kommen
+    public static double suche(double links, double rechts) throws Exception {
+        double half = (links + rechts) / 2;
+        double y1 = f(half);
+
+        if (zaehler <= maxIt) {
+            System.out.printf("Links %f Rechts %f Y %f Iteration %d\n", links, rechts, y1, zaehler);
+
+            if (Math.abs(links - rechts) < toleranz) {
+                System.out.println("Fertig");
+            } else {
+                if (y1 > 0) {
+                    zaehler++;
+                    return (suche(links, half));
+                } else if (y1 < 0) {
+                    zaehler++;
+                    return (suche(half, rechts));
+                }
+            }
+
+        } else {
+            throw new Exception("Zu viele Iterationen");
+        }
+
+        return half;
+    }
+}
